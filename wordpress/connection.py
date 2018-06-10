620d84d8bd3e7163168cd3c05b8bf9c7260125ef
Gianpiero Addis <gianpiero.addis@gmail.com>
2018-06-10 16:36:20 +0200

    Get the last saved version, update or insert the saved version

--- wordpress/connection.py
+++ wordpress/connection.py
@@ -1,4 +1,5 @@
 import sqlite3
+from datetime import datetime
 from instance import WpInstance
 
 
@@ -47,7 +48,7 @@ class WpDatabase():
                 INSERT INTO wp_instances (name, server, wp_path)
                 VALUES (:name, :server, :wp_path)
                 """,
-                           {'name': name, 'server': server, 'wp_path': wp_path})
+                {'name': name, 'server': server, 'wp_path': wp_path})
 
     def get_instances(self):
         """ Return a list containing all instances in the database. """
@@ -55,3 +56,39 @@ class WpDatabase():
             self.c.execute("SELECT * from wp_instances")
             instances = self.c.fetchall()
         return [WpInstance(*instance) for instance in instances]
+
+    def get_last_saved_version(self, instance_id):
+        """ Get the latest instance version saved in the database. """
+        with self.conn:
+            self.c.execute("""
+                SELECT id, version FROM wp_instances_versions
+                WHERE instance_id = ?
+                ORDER BY first_check DESC
+            """, (instance_id,))
+            return self.c.fetchone()
+
+    def update_version(self, instance_id, version):
+        """
+        Insert the new version for the given Wordpress instance ID,
+        or update the last_check timestamp for the current version.
+        """
+        with self.conn:
+            now = datetime.now()
+            last_saved_record = self.get_last_saved_version(instance_id)
+            if last_saved_record is not None:
+                last_saved_id = last_saved_record[0]
+                last_saved_version = last_saved_record[1]
+            else:
+                last_saved_version = None
+
+            if version != last_saved_version:
+                self.c.execute("""
+                    INSERT INTO wp_instances_versions (instance_id, version, first_check, last_check)
+                    VALUES (?, ?, ?, ?)
+                """, (instance_id, version, now, now))
+            else:
+                self.c.execute("""
+                    UPDATE wp_instances_versions
+                    SET last_check = ?
+                    WHERE id = ?
+                """, (now, last_saved_id))
--- wp-monitor.py
+++ wp-monitor.py
@@ -5,10 +5,13 @@ from wordpress.connection import WpDatabase
 
 def main():
     with WpDatabase('wp-monitor.db') as conn:
-        conn.insert_instance('LocalWp', 'user@localhost', 'test/wordpress-test')
+        # conn.insert_instance('Another WP Instance', 'seconduser@localhost', 'test/wordpress-test')
 
         for instance in conn.get_instances():
-            print "Name: {} Version: {}".format(instance.name, instance.check_version())
+            current_version = instance.check_version()
+            print "Name: {} Version: {}".format(instance.name, current_version)
+
+            conn.update_version(instance.key, current_version)
 
 if __name__ == '__main__':
     main()
